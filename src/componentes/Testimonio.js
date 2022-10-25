f49af6ad27099925a3c136c208b6fed0c30aea1e
MauricioBravo <qwe455r@gmail.com>
2022-10-25 16:34:51 -0300

    primer commit video youtube 1:39:57 seguir sin retroceder

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Testimonio(){ //un componente funcional es una funcion de js
+
+  return ( //un componente funcional de react retorna un elemento  de jsx
+    <div className='contenedor-testimonio'>
+        <img className='imagen-testimonio' src='../imagenes/Con los Pibes.jpg' alt='foto-con-amigos'/>
+    </div>
+  );    
+}
