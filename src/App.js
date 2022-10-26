b7a299ae4318c184594ca03ac30a2cc0d355d5e3
MauricioBravo <qwe455r@gmail.com>
2022-10-26 19:18:48 -0300

    2 ya con css, yt21555

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.js'; //en exportacion nombrada debemos ser más especificos y por eso usa {}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esta es mi página sobre Longboard</h1>
+      </div>
+
+      <Testimonio />
+      
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,10 +1,18 @@
 import React from "react";
+import '../style-sheets/Testimonio.css';
 
-function Testimonio(){ //un componente funcional es una funcion de js
+export function Testimonio(){ //un componente funcional es una funcion de js
 
   return ( //un componente funcional de react retorna un elemento  de jsx
     <div className='contenedor-testimonio'>
-        <img className='imagen-testimonio' src='../imagenes/Con los Pibes.jpg' alt='foto-con-amigos'/>
+        <img className='imagen-testimonio' src={require('../imagenes/Con los Pibes.jpg')} alt='foto con amigos'/>
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>Uno de los mejores días de mi vida</p>
+          <p className="historia-testimonio">Mis amigos, mi novia y la patineta</p>
+          <p className="texto-testimonio">Esta foto inmortaliza el día que cumplí un sueño, desde chico lo buscaba.</p> {/* si no anda probar con comillas simples */}
+        </div>
     </div>
   );    
 }
+
+ export default Testimonio; //esto está comentado para una exportacion nombrada, descomentado es uan exportacion por defecto
